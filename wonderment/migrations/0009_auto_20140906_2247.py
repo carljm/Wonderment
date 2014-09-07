e8c8041454a5fbbcde84701b9b48edcf001d6379
Carl Meyer <carl@oddbird.net>
2014-09-06 22:47:29 -0600

    Add migration.

---
+++ wonderment/migrations/0009_auto_20140906_2247.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wonderment', '0008_auto_20140906_2221'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='child',
+            options={'ordering': ['-birthdate']},
+        ),
+    ]
