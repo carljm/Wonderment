95667094ce6863417c38b5049c7c386f39e33969
Carl Meyer <carl@oddbird.net>
2015-08-31 00:33:55 -0600

    Remove CSV import.

--- wonderment/management/commands/import_families.py
+++
@@ -1,9 +0,0 @@
-from django.core.management import BaseCommand
-
-from wonderment import import_data, models
-
-
-class Command(BaseCommand):
-    def handle(self, *args, **options):
-        session = models.Session.objects.get(pk=args[1])
-        return import_data.import_csv(session, args[0])
