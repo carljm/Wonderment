cb2f30bdaaf08834d1b6b51106be13faf278ffec
Carl Meyer <carl@oddbird.net>
2016-01-22 12:52:40 -0700

    Catch up migrations.

---
+++ wonderment/migrations/0042_make_teacher_contact_info_optional.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wonderment', '0041_add_class_start_end_times'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='class',
+            options={'ordering': ['start'], 'verbose_name_plural': 'classes'},
+        ),
+        migrations.AlterField(
+            model_name='teacher',
+            name='email',
+            field=models.EmailField(blank=True, max_length=254),
+        ),
+        migrations.AlterField(
+            model_name='teacher',
+            name='name',
+            field=models.CharField(blank=True, max_length=100),
+        ),
+        migrations.AlterField(
+            model_name='teacher',
+            name='phone',
+            field=models.CharField(blank=True, max_length=25),
+        ),
+    ]
